7c85e2cde5565e3d0a6e37bb04b969d338c9dac0
Devi Yadlapalli <devi@bijib.in>
2026-04-20 18:24:33 +0530

    jenkis sh files added

---
+++ Nepean - invoice -Live /Run-JMeter-Jenkins.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "Running JMeter in Jenkins..."
+
+# ✅ Memory settings
+export HEAP="-Xms768m -Xmx1g"
+
+# Properties
+JMETER_PROP=""
+JMETER_PROP="$JMETER_PROP -Djmeter.protocol=https"
+JMETER_PROP="$JMETER_PROP -Djmeter.serverName=portal.bijib.com"
+JMETER_PROP="$JMETER_PROP -Djmeter.serverPort=443"
+JMETER_PROP="$JMETER_PROP -Djmeter.users=1"
+JMETER_PROP="$JMETER_PROP -Djmeter.duration=1"
+JMETER_PROP="$JMETER_PROP -Djmeter.loopCount=1"
+
+echo "Executing test..."
+
+jmeter -n \
+  -t "scripts/nepean-invoice-generation-checking.jmx" \
+  -l results/result.jtl \
+  $JMETER_PROP
